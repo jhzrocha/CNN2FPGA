585d8a476693058b5e86799e0b32411b91e9f524
jhzrocha <joaoheitorzabel1808@gmail.com>
2024-04-17 20:11:01 -0300

    [28][jhzrocha] Finishing Component declarations and signals

--- File Converter/converter.py
+++ File Converter/converter.py
@@ -6,7 +6,9 @@ class Converter():
     
     entityStartLine = 0
     entityEndLine = 0
-    
+
+    librariesAndUses = []
+
     genericStartLine= 0
     genericEndLine = 0
     generics = []
@@ -31,6 +33,7 @@ class Converter():
         self.lines = open(filePath,'r').readlines()
         self.removeCommentLines()
         self.defineEntity()
+        self.setLibrariesAndUses()
         self.defineArchitecture()
     
     def __str__(self):
@@ -59,11 +62,18 @@ class Converter():
 
 
 '''
+    
+    def setLibrariesAndUses(self):
+        for line in self.lines:
+            if(line.find('use') != -1 or line.find('library') != -1):
+                self.librariesAndUses.append(line)
+
     def removeCommentLines(self):
         i = 0
         while i < len(self.lines):
             if (self.hasComment(self.lines[i]) and self.lines[i].strip().find('--') < 1):
                 self.lines.pop(i)
+                i = i-1
             i = i + 1
 
     def hasComment(self,line):
@@ -95,7 +105,7 @@ class Converter():
         for line in self.lines[self.entityStartLine:self.entityEndLine]:        
             if line.find('generic') != -1 :
                 self.genericStartLine = counter
-                break       
+                break
             counter = counter + 1
         for line in self.lines[self.genericStartLine:self.entityEndLine]:        
             generic = []
@@ -108,38 +118,25 @@ class Converter():
                     if (len(generic) == 3):
                         generic[2] = generic[2].replace('=','')
                     else:
-                        generic.append('')
-                    self.generics.append(generic)       
+                       if (generic[1].find('\n') != -1):
+                           generic[1] = generic[1].replace('\n','')
+                       generic.append('')
+                    self.generics.append(generic)
             counter = counter + 1
     
     def setPortmap(self):
         counter = self.genericEndLine
         for line in self.lines[self.genericEndLine:self.entityEndLine]:        
-            if line.find('port') != -1 :
+            if not self.isInLine('port',line):
                 self.portmapStartLine = counter
                 break       
             counter = counter + 1
         for line in self.lines[self.portmapStartLine:self.entityEndLine]:        
-            port = []
-            if line.find(':') != -1:
-                if (line.find('in') != -1):
-                    port = line.replace(' in ','').split(';')[0].split(':')
-                    if (len(port) == 3):
-                        port[2] = port[2].replace('=','',1)
-                    else:
-                        port.append('')
-                    
-                    self.strip(port)
-
-                    self.portmapIn.append(port)
-                if (line.find('out') != -1):
-                    port = line.replace('\n','').replace(' out ','').split(';')[0].split(':')
-                    if (len(port) == 3):
-                        port[2] = port[2].replace('=','')
-                    else:
-                        port.append('')
-                    self.strip(port)
-                    self.portmapOut.append(port)
+            if ':' in line:
+                if (self.isInLine('in',line)):
+                    self.addInputPort(line)
+                elif (self.isInLine('out',line)):
+                    self.addOutputPort(line)
             counter = counter + 1
     
     def iniciateInternalComponents(self):
@@ -147,7 +144,7 @@ class Converter():
                 self.internalComponents[usedComponent] = {}
 
     def isArchitectureStartLine(self, line):
-        return True if line.find('architecture') != -1 else False
+        return True if 'architecture' in line else False
 
     def setArchitectureFirstLine(self):
         for index in range(self.entityEndLine,len(self.lines)-1):
@@ -159,32 +156,17 @@ class Converter():
     def setArchitectureDeclarationEndLine(self):
         for index in range(self.architectureStartLine,len(self.lines)-1):
             line = self.lines[index]
-            if (line.find('begin') != -1):
+            if 'begin' in line:
                 self.architectureDeclarationEndLine = index
                 break
 
     def setArchitectureDeclarations(self):
-        self.setArchitectureDeclarationEndLine()
-
+        self.setGenericComponentDeclarations()
+        self.setSignalDeclarations()
+        
+    def setSignalDeclarations(self):
         for index in range(self.architectureStartLine,self.architectureDeclarationEndLine):
             line = self.lines[index]            
-            
-            if(line.find('component') != -1 and line.find('component;') == -1):
-                componentname = line.strip().split(' ')[1]
-                self.usedComponents[componentname] = {}
-                internalIndex = index
-                internalLine = self.lines[internalIndex]
-                while internalLine.find('end component;') != -1:
-
-                    if (internalLine.find('generic') != -1):
-                        self.usedComponents[componentname]['generic'] ={}
-                        while internalLine.find(');') != -1:                            
-                            print(internalLine)
-                            self.usedComponents[componentname]['generic'][internalLine.split(':')[0].strip()] = internalLine.split(':')[2].replace('=','').strip()
-                            internalIndex = internalIndex + 1
-                            internalLine = self.lines[internalIndex]
-
-                    internalIndex = internalIndex + 1
             
             if(line.find('signal') != -1):
                 signal = line.replace('signal','').strip().split(';')[0].split(':')
@@ -195,14 +177,64 @@ class Converter():
                 
                 signal[0] = signal[0].strip()
                 signal[1] = signal[1].strip()
-                self.internalSignal.append(signal)
+                self.internalSignal.append(signal)    
+    
+    def setGenericComponentDeclarations(self):
+        for index in range(self.architectureStartLine,self.architectureDeclarationEndLine):
+            line = self.lines[index]
+            if('component' in line and 'component;' not in line):
+                componentname = line.strip().split(' ')[1]
+                self.usedComponents[componentname] = {}
+                internalIndex = index
+                internalLine = self.lines[internalIndex]
+                while 'end component;' not in internalLine.lower():
+                    if ('generic ' in internalLine.lower() or 'generic(' in internalLine.lower() or ' generic(' in internalLine.lower()):                    
+                        genericLines = ''
+                        self.usedComponents[componentname]['generic'] = []
+                        while 'port' not in internalLine and 'PORT' not in internalLine and 'Port' not in internalLine:
+                            genericLines = genericLines + internalLine
+                            internalIndex = internalIndex + 1
+                            internalLine = self.lines[internalIndex]
+                        genericLines = genericLines.replace('\n','')[genericLines.find('(') + 1:genericLines.find(');')]
+                        generics = genericLines.strip().replace(');','').split(';')
+                        for generic in generics:
+                            addedGeneric = generic.split('--')[0].split(':')
+                            addedGenericName = addedGeneric[0].strip()
+                            addedGenericType = addedGeneric[1].strip()
+                            addedGenericValue = addedGeneric[2].replace('=','').strip()
+                            self.usedComponents[componentname]['generic'].append([addedGenericName,addedGenericType,addedGenericValue])
+                        index = internalIndex
+                    if('port' in internalLine.lower()):
+                        self.usedComponents[componentname]['portmap'] = {}
+                        self.usedComponents[componentname]['portmap']['in'] = []
+                        self.usedComponents[componentname]['portmap']['out'] = []
+                        portmapLines = ''
+                        while ':' in internalLine or ');' not in internalLine:
+                            portmapLines = portmapLines + internalLine
+                            internalIndex = internalIndex + 1
+                            internalLine = self.lines[internalIndex]
+                        portmaps = portmapLines.replace('PORT','port').replace('\n','').replace('\t','').replace('port','').replace('(','',1).split(';')
+                        for port in portmaps:
+                            portmap = port.split('--')[0].split(':')
+                            portName = portmap[0].strip()
+                            portType = portmap[1]
+                            portValue = ''
+                            if len(portmap) == 3:
+                                portValue = portmap[2].replace('=','',1)
+                            
+                            if ' in ' in port.lower():
+                                self.usedComponents[componentname]['portmap']['in'].append([portName,portType.replace(' in','').replace(' IN','').strip(),portValue.strip()])
+
+                            if ' out ' in port.lower():
+                                self.usedComponents[componentname]['portmap']['out'].append([portName,portType.replace(' out','').replace(' OUT','').strip(),portValue])
+
+                    internalIndex = internalIndex + 1
+                    internalLine = self.lines[internalIndex]
+    
     def defineArchitecture(self):
-
         self.setArchitectureFirstLine()
-        self.setArchitectureDeclarations()
-
-
-        
+        self.setArchitectureDeclarationEndLine()
+        self.setArchitectureDeclarations()        
         self.iniciateInternalComponents()
         for index in range(self.architectureDeclarationEndLine,len(self.lines)-1):
             line = self.lines[index]
@@ -237,3 +269,26 @@ class Converter():
         self.setEntityEndLine()        
         self.setGeneric()
         self.setPortmap()
+
+    def isInLine(self, substring, line):
+        if substring.lower() in line.lower():
+            return True
+        return False
+    
+    def addInputPort(self,line):
+        port = line.replace(' in ','').replace(' IN ','').split(';')[0].split(':')        
+        if (len(port) == 3):
+            port[2] = port[2].replace('=','',1)
+        else:
+            port.append('')                    
+        self.strip(port)
+        self.portmapIn.append(port)
+
+    def addOutputPort(self,line):
+        port = line.replace('\n','').replace(' out ','').replace(' OUT ','').split(';')[0].split(':')
+        if (len(port) == 3):
+            port[2] = port[2].replace('=','')
+        else:
+            port.append('')
+        self.strip(port)
+        self.portmapOut.append(port)
